e0bcce8cb9412b0df865cf55cc501109661ce2a3
Leslie <leslie.ovid@gmail.com>
2015-11-03 02:51:28 -0500

    Updated

--- app/src/main/java/com/app/i2i/lookout/FragmentMainActivity.java
+++ app/src/main/java/com/app/i2i/lookout/FragmentMainActivity.java
@@ -6,18 +6,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 public class FragmentMainActivity extends Fragment {
 
-
+    Integer images[] = {R.drawable.missing, R.drawable.missing1, R.drawable.missing};
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v, c;
         v = inflater.inflate(R.layout.fragment_main, container, false);
 
 
+        addImagesToThegallery(v);
+
         return v;
     }
 
+    private void addImagesToThegallery(View c) {
+        LinearLayout imageGallery = (LinearLayout) c.findViewById(R.id.imageGallery);
+        for (Integer image : images) {
+            imageGallery.addView(getImageView(image));
+
+            imageGallery.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity(), "item clicked", Toast.LENGTH_LONG).show();
+                }
+            });
+
+        }
+
+
+    }
+
+
+    private View getImageView(Integer image) {
+        ImageView imageView = new ImageView(getContext());
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(0, 0, 0, 0);
+        lp.gravity = 0;
+        lp.weight = 1;
+        imageView.setLayoutParams(lp);
+        imageView.setImageResource(image);
+        return imageView;
+    }
 
 }
---
+++ app/src/main/java/com/app/i2i/lookout/models/NewsModel.java
@@ -0,0 +1,13 @@
+package com.app.i2i.lookout.models;
+
+
+public class NewsModel {
+    public static final String[] data = {"Crime - Break in", "Donut", "Eclair",
+            "Froyo", "Gingerbread", "Honeycomb",
+            "Icecream Sandwich", "Jelly Bean", "Kitkat", "Lollipop"};
+    public String name;
+
+    NewsModel(String name) {
+        this.name = name;
+    }
+}
